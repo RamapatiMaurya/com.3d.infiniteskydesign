22f44125fcf4a1d13918a7f5bf161b0b400b4bb1
Ramapati Maurya <mauryaramapati@gmail.com>
2020-12-12 08:48:46 +0530

    Theme upgradation
    Changes theme

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+router.use(express.static(__dirname+"./public"))
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', 
+  subtitle1: 'A responsive video gallery template with a functional designed by', subtitle2: '', url: '' });
 });
 
 module.exports = router;
