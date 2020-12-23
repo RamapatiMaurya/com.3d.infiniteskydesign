27afc20e44a0152c4c2b2ac6116fef9f354c8a3b
Ramapati Maurya <mauryaramapati@gmail.com>
2020-12-23 22:50:21 +0530

    upgrade upload page design
    upgrade upload page design

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 var express = require('express');
-
+var path = require('path')
 var multer  = require('multer')
-var upload = multer({ dest: 'uploads/' })
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+   // cb(null, file.fieldname + '-' + Date.now())
+    cb(null, file.originalname)
+  }
+})
+
+//var upload = multer({ dest: 'uploads/' })
+var upload = multer({ storage: storage })
 
 var router = express.Router();
 var connection = require('../modules/connection')
